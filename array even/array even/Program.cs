1f586c4dcfb77c97018613059873ae8d87ca0496
user-ПК\user <IPRIT92@MAIL.RU>
2016-09-12 16:43:49 +0300

    working wersion

--- array even/array even/Program.cs
+++ array even/array even/Program.cs
@@ -8,8 +8,46 @@ namespace array_even
 {
     class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
+            int input_size;
+            Console.WriteLine("Введите число");
+            while (!int.TryParse(Console.ReadLine(), out input_size ))
+                {
+                    Console.WriteLine("Не корректное число");
+                }
+
+            int[] sample = new int[input_size];
+            Console.WriteLine("Введите число");
+            int input;
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Не корректное число");
+            }
+
+            int i,j=0;
+            for (i = 0; i < input_size; i ++)
+            {
+                if (input % 2 == 0)
+                {
+                    sample[j] = input;
+                    j++;
+                }
+                else
+                {
+                    i--;
+                }
+
+                input ++;
+                
+            }
+
+            for (i = 0; i < input_size; i++)
+            {
+                Console.WriteLine("sample[" + i + "]: " + sample[i]);
+            }
+            Console.ReadLine();
         }
+
     }
 }
